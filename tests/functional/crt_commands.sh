7aa3e7495763300aa9e87aeb140d3f80f8284b8b
aiqin <yuhongli@globrealwide-lm.peking.corp.yahoo.com>
2012-12-24 18:30:23 +0800

    add function tests

---
+++ tests/functional/crt_commands.sh
@@ -0,0 +1,14 @@
+#Test click button (using stay=true) and click a link
+arrow data/crt_test/crt_descriptor1.json --driver=crt --report=true
+# button id is wrong, should prompt error and case fail
+arrow data/crt_test/crt_descriptor2.json --driver=crt --report=true
+# link id is wrong, should prompt error anc case fail
+arrow data/crt_test/crt_descriptor3.json --driver=crt --report=true
+# test 2 tests on 2 clients, need launch 2 clients
+arrow data/crt_test/crt_descriptor4.json --driver=crt --report=true
+# test 2 tests on 1 client, need launch 1 client
+arrow data/crt_test/crt_descriptor4.json --driver=crt --report=true
+# test 1 test on 2 clients, need launch 2 clients
+arrow data/crt_test/crt_descriptor5.json --driver=crt --report=true
+# test 1 test on 1 client, need launch 1 client
+arrow data/crt_test/crt_descriptor5.json --driver=crt --report=true
