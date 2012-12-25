25fa278cd8c1a8fde98dd9eab24477b74ae4952f
aiqin <yuhongli@globrealwide-lm.peking.corp.yahoo.com>
2012-12-25 18:19:52 +0800

    update stub crtwebdriver.js

--- tests/unit/stub/crtwebdriver.js
+++ tests/unit/stub/crtwebdriver.js
@@ -12,12 +12,12 @@ crtWebdriver.prototype.buildWebDriver = function (sid, callback)
 {
  var self = this;
  self.sessionId=sid;
- this.controller = this.createController(); 
- this.controller.connect();
+ // this.controller = this.createController(); 
+ // this.controller.connect();
  this.actions = [];
- this.controller.on('local.ready', function() {
-    callback();
- });
+ // this.controller.on('local.ready', function() {
+    // callback();
+ // });
            
 }
 
@@ -42,23 +42,21 @@ crtWebdriver.prototype.executeScript = function (script, callback) {
 crtWebdriver.prototype.navigate = function (url, callback) {
     // this.controller.navigate(this.sessionId, url, callback);
     this.actions.push({"name": "navigate","sessionId":this.sessionId, "url":url, "callback":callback});
-    callback();
 }
 
 crtWebdriver.prototype.clickAndStay = function (locator, callback) {
     var code = "var browserbot = new MozillaBrowserBot(window);\n" + 
         "var selenium = new Selenium(browserbot);\n" +
-        "selenium.doClick(\"id=" + locator + "\");\n";
-
-    this.actions.push({"name": "clickAndStay","sessionId":this.sessionId, "code":code, "callback":callback});
+        "selenium.doClick(\"" + locator + "\");\n";
+    this.actions.push({"name": "clickAndStay","sessionId":this.sessionId, "code":code, "callback":callback()});
 }
 
 crtWebdriver.prototype.clickAndNavigate = function (locator, callback) {
     var code = "var browserbot = new MozillaBrowserBot(window);\n" + 
         "var selenium = new Selenium(browserbot);\n" +
-        "selenium.doClick(\"id=" + locator + "\");\n";
+        "selenium.doClick(\"" + locator + "\");\n";
     
-    this.actions.push({"name": "clickAndNavigate","sessionId":this.sessionId, "code":code, "callback":callback});
+    this.actions.push({"name": "clickAndNavigate","sessionId":this.sessionId, "code":code, "callback":callback()});
 }
 
 module.exports = crtWebdriver;
