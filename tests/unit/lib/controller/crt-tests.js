eb65677772950b5394cb098cf76785fea4485339
aiqin <yuhongli@globrealwide-lm.peking.corp.yahoo.com>
2012-12-26 15:30:20 +0800

    add ut for crt controller

--- tests/unit/lib/controller/crt-tests.js
+++ tests/unit/lib/controller/crt-tests.js
@@ -6,7 +6,8 @@
 
 YUI.add('crt-tests', function(Y, NAME) {
 
-    var path = require('path'), arrowRoot = path.join(__dirname, '../../../..'), crtController = require(arrowRoot + '/lib/controller/crt.js'), StubDriver = require(arrowRoot + '/tests/unit/stub/driver.js');
+    var path = require('path'), arrowRoot = path.join(__dirname, '../../../..'), 
+    crtController = require(arrowRoot + '/lib/controller/crt.js'), StubDriver = require(arrowRoot + '/tests/unit/stub/driver.js');
     StubWdApp = require(arrowRoot + '/tests/unit/stub/crtwebdriver.js');
     suite = new Y.Test.Suite(NAME), A = Y.Assert, self = this;
 
@@ -31,7 +32,7 @@ YUI.add('crt-tests', function(Y, NAME) {
 
     suite.add(new Y.Test.Case({
 
-        'test click button locator' : function() {
+        'test click button' : function() {
             var locator = "id=adjust";
             var sessionId = "1356313637866-211-client";
             var code = "var browserbot = new MozillaBrowserBot(window);\n" + "var selenium = new Selenium(browserbot);\n" + "selenium.doClick(\"" + locator + "\");\n";
@@ -45,8 +46,7 @@ YUI.add('crt-tests', function(Y, NAME) {
             A.isTrue(code === actions[0].code, 'selenium code for clicking button should be the same');
             A.isTrue("null" === self.callbackMsg, 'callback is not called');
         },
-
-        'test click link locator' : function() {
+        'test click link' : function() {
             var locator = "id=testlink";
             var sessionId = "1356313637866-211-client";
             var code = "var browserbot = new MozillaBrowserBot(window);\n" + "var selenium = new Selenium(browserbot);\n" + "selenium.doClick(\"" + locator + "\");\n";
@@ -59,18 +59,77 @@ YUI.add('crt-tests', function(Y, NAME) {
             A.isTrue(code === actions[0].code, 'selenium code for clicking button should be the same');
             A.isTrue("null" === self.callbackMsg, 'callback is not called');
         },
+
+        'test input text' : function() {
+            var locator = "//input[@id='text1']", value = "hello"
+            var sessionId = "1356313637866-211-client";
+            var code = "var browserbot = new MozillaBrowserBot(window);\n" + "var selenium = new Selenium(browserbot);\n" + "selenium.doType(\"" + locator + "\",\"" + value + "\"); answerFn({});\n";
+            var actions = validateLocator("1356313637866-211-client", {
+                "using" : "xpath",
+                "value" : "//input[@id='text1']",
+                "text" : "hello"
+            });
+            A.isTrue('typeText' === actions[0].name, 'type text shuold use typeText method');
+            A.isTrue(sessionId === actions[0].sessionId, 'sessionId pass wrong');
+            A.isTrue(code === actions[0].code, 'selenium code for typing text should be the same');
+            A.isTrue("null" === self.callbackMsg, 'callback is not called');
+        },
+        
         'test no value' : function() {
             var actions = validateLocator("1356313637866-211-client", {
                 page : 'http://localhost:8000/index.html'
             });
             A.isTrue("\"value\" parameter is required" === self.callbackMsg, 'Must have value');
+        },
+        'test using strategy - link' : function() {
+            var locator = "link=Inner page";
+            var sessionId = "1356313637866-211-client";
+            var code = "var browserbot = new MozillaBrowserBot(window);\n" + "var selenium = new Selenium(browserbot);\n" + "selenium.doClick(\"" + locator + "\");\n";
+            var actions = validateLocator("1356313637866-211-client", {
+                using : "link",
+                value : "Inner page",
+                click : true
+            });
+            A.isTrue('clickAndNavigate' === actions[0].name, 'click a link shuold use clickAndNavigate method');
+            A.isTrue(sessionId === actions[0].sessionId, 'sessionId pass wrong');
+            A.isTrue(code === actions[0].code, 'selenium code for clicking button should be the same');
+            A.isTrue("null" === self.callbackMsg, 'callback is not called');
+        },
+        'test using strategy - xpath' : function() {
+            var locator = "//a[@id='testlink']";
+            var sessionId = "1356313637866-211-client";
+            var code = "var browserbot = new MozillaBrowserBot(window);\n" + "var selenium = new Selenium(browserbot);\n" + "selenium.doClick(\"" + locator + "\");\n";
+            var actions = validateLocator("1356313637866-211-client", {
+                using : "xpath",
+                value : "//a[@id='testlink']",
+                click : true
+            });
+            A.isTrue('clickAndNavigate' === actions[0].name, 'click a link shuold use clickAndNavigate method');
+            A.isTrue(sessionId === actions[0].sessionId, 'sessionId pass wrong');
+            A.isTrue(code === actions[0].code, 'selenium code for clicking button should be the same');
+            A.isTrue("null" === self.callbackMsg, 'callback is not called');
+        },
+        'test using invalid strategy' : function() {
+            var locator = "now=//a[@id='testlink']";
+            var sessionId = "1356313637866-211-client";
+            var code = "var browserbot = new MozillaBrowserBot(window);\n" + "var selenium = new Selenium(browserbot);\n" + "selenium.doClick(\"" + locator + "\");\n";
+            var actions = validateLocator("1356313637866-211-client", {
+                using : "now",
+                value : "//a[@id='testlink']",
+                click : true
+            });
+            A.isTrue('clickAndNavigate' === actions[0].name, 'click a link shuold use clickAndNavigate method');
+            A.isTrue(sessionId === actions[0].sessionId, 'sessionId pass wrong');
+            A.isTrue(code === actions[0].code, 'selenium code for clicking button should be the same');
+            A.isTrue("execError: Unrecognised locator type: 'now'" === self.callbackMsg, 'callback is not called');
+        },
+        'test no click and text' : function() {
+            var actions = validateLocator("1356313637866-211-client", {
+                using : "xpath",
+                value : "//a[@id='testlink']"
+            });
+            A.isTrue("No support params 'click' or 'text' found" === self.callbackMsg, 'callback is not called');
         }
-        // 'test no stay for clicking button' : function() {
-// 
-        // },
-        // 'test no click button' : function() {
-// 
-        // }
        
     }));
 
