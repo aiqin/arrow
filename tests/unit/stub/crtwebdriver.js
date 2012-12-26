27ec1e8e8be94d1eafa8cdd3c5015e92a766fe7d
aiqin <yuhongli@globrealwide-lm.peking.corp.yahoo.com>
2012-12-26 15:30:59 +0800

    add type text operation in stub

--- tests/unit/stub/crtwebdriver.js
+++ tests/unit/stub/crtwebdriver.js
@@ -44,19 +44,31 @@ crtWebdriver.prototype.navigate = function (url, callback) {
     this.actions.push({"name": "navigate","sessionId":this.sessionId, "url":url, "callback":callback});
 }
 
-crtWebdriver.prototype.clickAndStay = function (locator, callback) {
+crtWebdriver.prototype.clickAndStay = function (locator, callback,errcallback) {
     var code = "var browserbot = new MozillaBrowserBot(window);\n" + 
         "var selenium = new Selenium(browserbot);\n" +
         "selenium.doClick(\"" + locator + "\");\n";
     this.actions.push({"name": "clickAndStay","sessionId":this.sessionId, "code":code, "callback":callback()});
 }
 
-crtWebdriver.prototype.clickAndNavigate = function (locator, callback) {
+crtWebdriver.prototype.clickAndNavigate = function (locator, callback,errcallback) {
     var code = "var browserbot = new MozillaBrowserBot(window);\n" + 
         "var selenium = new Selenium(browserbot);\n" +
         "selenium.doClick(\"" + locator + "\");\n";
     
-    this.actions.push({"name": "clickAndNavigate","sessionId":this.sessionId, "code":code, "callback":callback()});
-}
+    if(locator.indexOf("now")!==-1){//invalid locator
+         this.actions.push({"name": "clickAndNavigate","sessionId":this.sessionId, "code":code, "callback":callback(),"errcallback":errcallback("execError: Unrecognised locator type: 'now'")});
+    }else{
+         this.actions.push({"name": "clickAndNavigate","sessionId":this.sessionId, "code":code, "callback":callback()});   
+    }
 
+}
+crtWebdriver.prototype.typeText = function (locator, value, callback,errback) {
+    var code = "var browserbot = new MozillaBrowserBot(window);\n" + 
+        "var selenium = new Selenium(browserbot);\n" +
+        "selenium.doType(\"" + locator +"\",\""+value+ "\"); answerFn({});\n";
+     this.logger.info("[Step] Type text:locator:"+locator+":value:"+value);
+     this.actions.push({"name": "typeText","sessionId":this.sessionId, "code":code, "callback":callback()});   
+
+}
 module.exports = crtWebdriver;
